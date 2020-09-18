9250ab982e9059333f0690841596bc905ab74ed0
AshishBikram <ashish.lamichhane@logicabeans.com>
2020-09-18 11:57:49 +0545

    changes in the form rendering

--- wp-content/plugins/cartapari-survey/cartapari-survey.php
+++ wp-content/plugins/cartapari-survey/cartapari-survey.php
@@ -21,8 +21,9 @@ require_once(ABSPATH . 'wp-config.php');
 require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 require_once( ABSPATH . 'wp-content/plugins/cartapari-survey/util/install.php');
 require_once( ABSPATH . 'wp-content/plugins/cartapari-survey/util/form_render.php');
+require_once( ABSPATH . 'wp-content/plugins/cartapari-survey/util/form_rendering.php');
 
-
+add_action("wp_enqueue_scripts","render_css");
 add_action("admin_menu", "addMenu");
 function addMenu(){
     add_menu_page("CartaPari Survey", "CartaPari Survey", "edit_pages", "cartapari-survey", "cartapariSurvey");
@@ -36,7 +37,12 @@ function cartapariSurvey(){
 function exportCartapariSurveyData(){
     echo "Exporting..";
     surveyForm();
+
 }
 
 register_activation_hook( __FILE__, 'createRequiredTables' );
 register_deactivation_hook( __FILE__, 'drop_tables' );
+
+function render_css(){
+    wp_register_style('render',get_template_directory_uri() . '/assets/css/formRenter');
+}
