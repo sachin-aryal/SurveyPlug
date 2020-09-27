8219c971c3984cef98e69a4551dab0ce93b3c0d5
AshishBikram <ashish.lamichhane@logicabeans.com>
2020-09-28 01:51:21 +0545

    add data in excel

---
+++ wp-content/plugins/cartapari-survey/util/_add_data_excel.php
@@ -0,0 +1,150 @@
+<?php
+
+require_once('constant.php');
+require_once( ABSPATH . 'wp-content/plugins/cartapari-survey/vendor/autoload.php');
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Writer\Pdf\Mpdf;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+
+function create_pdf_survey_report($rating){
+    $reader = IOFactory::createReader("Xlsx");
+    $spreadsheet = $reader->load(ABSPATH . 'wp-content/plugins/cartapari-survey/assets/form_render_template.xlsx');
+    
+    add_company_info($spreadsheet);
+    add_select_yes_no($spreadsheet);
+    add_partial_rating($spreadsheet, $rating[0]);
+    add_total_rating($spreadsheet, $rating[1]);
+    add_answer_note($spreadsheet);
+    
+    $spreadsheet ->getDefaultStyle()->applyFromArray(
+        [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                    'color' => ['rgb' => '000000'],
+                ],
+            ]
+        ]
+    );
+    $xmlWriter = IOFactory::createWriter($spreadsheet,'Mpdf');
+    
+    $xmlWriter->writeAllSheets();
+    $num = rand(00, 99);
+    $xmlWriter->save(ABSPATH . 'wp-content/plugins/cartapari-survey/assets/helloWorld$num.pdf');
+    
+   // $writer = IOFactory::createWriter($spreadsheet, "Xlsx");
+    //$writer->save(ABSPATH . 'wp-content/plugins/cartapari-survey/assets/05featuredemo.xlsx');
+}
+
+function add_partial_rating($spreadsheet, $partial_rating){
+    global $index_to_partial_rating;
+    for($i = 0; $i < count($index_to_partial_rating); $i++){
+        $j = $i - 1;
+        if(reset($index_to_partial_rating) === $index_to_partial_rating[$i]){
+            $row_num = $index_to_partial_rating[$i] + 16;
+            $spreadsheet->getActiveSheet()
+            ->getCell('H' . $row_num)
+            ->setValue($partial_rating[$i] . '%');
+        }else{
+            if($index_to_partial_rating[$i] - $index_to_partial_rating[$j] === 1){
+                $row_num = $index_to_partial_rating[$i] + 16;
+                $spreadsheet->getActiveSheet()
+                ->getCell('H' . $row_num)
+                ->setValue($partial_rating[$i] . '%');
+            }else{
+                $row_num = $index_to_partial_rating[$i] + 16;
+                $last_row_num = $index_to_partial_rating[$j] + 17;
+                
+                $spreadsheet->getActiveSheet()
+                    ->getCell('H' . $last_row_num)
+                    ->setValue($partial_rating[$i] . '%');
+                
+            }
+        }
+        
+    }
+}
+
+function add_total_rating($spreadsheet, $total_rating){
+    global $index_to_total_excel;
+    for($i = 0; $i < count($index_to_total_excel); $i++){
+        $row_num = $index_to_total_excel[$i] + 16;
+        $spreadsheet->getActiveSheet()
+        ->getCell('I' . $row_num)
+        ->setValue($total_rating[$i] . "%");
+    }   
+}
+
+function add_answer_note($spreadsheet){
+    global $index_to_note2;
+    foreach($_POST as $key => $value){
+        $note = explode('_',$key);
+        
+        if(($note[0] == 'note')){
+            
+            $row_num = 16 + $index_to_note2[(int)$note[1]-1];
+            if($row_num === 75){
+                $spreadsheet->getActiveSheet()
+                ->getCell('G' . $row_num)
+                ->setValue($value);
+            }else{
+                $spreadsheet->getActiveSheet()
+                ->getCell('J' . $row_num)
+                ->setValue($value);
+            }
+            
+        }  
+        
+    }
+}
+
+function add_select_yes_no($spreadsheet){
+    foreach($_POST as $key => $value){
+        $select = explode('_',$key);
+        
+        if(($select[0] == 'select')){
+            $row_num = 16 + (int)$select[1];
+            if($row_num < 74){
+                $spreadsheet->getActiveSheet()
+                    ->getCell('F' . $row_num)
+                    ->setValue($value);
+            }else{
+                $row_num = $row_num + 1;
+                $spreadsheet->getActiveSheet()
+                    ->getCell('F' . $row_num)
+                    ->setValue($value);
+            }
+            
+        }       
+    }
+}
+
+function add_company_info($spreadsheet){
+    $spreadsheet->getActiveSheet()
+        ->getCell('B4')
+        ->setValue($_POST['company_name']);
+    $spreadsheet->getActiveSheet()
+        ->getCell('B5')
+        ->setValue($_POST['company_type']);
+    $spreadsheet->getActiveSheet()
+        ->getCell('B6')
+        ->setValue($_POST['sector']);
+    $spreadsheet->getActiveSheet()
+        ->getCell('B7')
+        ->setValue($_POST['no_of_employee']);
+    $spreadsheet->getActiveSheet()
+        ->getCell('B8')
+        ->setValue($_POST['state']);
+    $spreadsheet->getActiveSheet()
+        ->getCell('B9')
+        ->setValue($_POST['author']);
+    $spreadsheet->getActiveSheet()
+        ->getCell('B10')
+        ->setValue($_POST['author_email']);
+    $spreadsheet->getActiveSheet()
+        ->getCell('B11')
+        ->setValue($_POST['issued_date']);
+}
+?>
